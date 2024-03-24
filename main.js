2c3af66e9125515688da76b923e9796a7e3f50a6
raditanurhasna <raditanurhasna1410@gmail.com>
2024-03-24 19:31:07 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function changeImage()
+{
+    var img = document.getElementById('image');
+    img.src='assests/img4.jpeg'
+    width="800" 
+    height="400" 
+    style="border-radius: 20px"
+    Class="Notes"
+    id="image" ;  
+}
+
+let tanggal = new Date();
+document.getElementById("hasil").innerHTML = tanggal;
+
+
+
+
+
